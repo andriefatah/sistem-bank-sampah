75db60b8beca41094a6dc513964ffc7eff7b984a
Andrie Adzkia Fatah <andrieadzkia17@gmail.com>
2024-12-15 07:39:32 +0700

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+            <p>Sistem Manajamen Bank Sampah Rendole Pati</p>
+        </nav>
+    </div>
+</footer>
